d58b31b1087e461baa636f79384a25ccb92859e0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 11:25:00 -0800

    dry sharing config between cli and pattern

--- bin/share_config.sh
+++ bin/share_config.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-ESCAPE_CLI_PATTERN_DIRECTORY_IN_PARENT_S_NODE_MODULES="../../../"
-
-share_file() {
-	FILE="$1"
-	cp ${FILE} ${ESCAPE_CLI_PATTERN_DIRECTORY_IN_PARENT_S_NODE_MODULES}${FILE}
-}
-
-mkdir -p ${ESCAPE_CLI_PATTERN_DIRECTORY_IN_PARENT_S_NODE_MODULES}test
-
-share_file Makefile.snapshot
-share_file test/snapshot.test.ts
+make share_file FILE=Makefile.snapshot
+make share_file FILE=test/snapshot.test.ts
