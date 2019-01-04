e79b507636afe179c0804a255f43940884270196
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 11:50:23 -0800

    hopefully DRY now sharing files - cmon now - ok undo

--- bin/share_config.sh
+++ bin/share_config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-. ../../../node_modules/@musical-patterns/cli/bin/non_cli/share_file.sh
+. node_modules/@musical-patterns/cli/bin/non_cli/share_file.sh
 
 share_file Makefile.snapshot
 share_file test/snapshot.test.ts
