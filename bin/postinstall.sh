e27b3be5242e0582efc42b16708ca6133540170d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 17:08:25 -0800

    1.0.58: i really feel like this should work

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-. ../../../node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh
+. node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh
 
 run_only_if_not_self_installing "bash ./bin/share_config.sh"
