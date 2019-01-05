4aa48f7551051ffdc11c41765b527be973011644
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 17:50:17 -0800

    1.0.65: just so we dont experience this again, set +e

--- bin/share_config.sh
+++ bin/share_config.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
+set +e
 
-echo "a"
 . ../../../node_modules/@musical-patterns/cli/bin/non_cli/share_file.sh
-echo "b"
 share_file Makefile.snapshot
-echo "c"
 share_file test/snapshot.test.ts
-echo "d"
