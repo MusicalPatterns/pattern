4d5049d4dd0a13cd3be7a37a602bd7fab49c1fc8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 17:48:30 -0800

    1.0.64: now debugging installation into patterns

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -2,8 +2,5 @@
 
 . ../../../node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh
 if [[ $? == 0 ]] ; then
-	echo "made it in here"
 	run_only_if_not_self_installing "bash ./bin/share_config.sh"
-else
-	echo "not really sure what to do here honestly"
 fi
--- bin/share_config.sh
+++ bin/share_config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-echo "am i running"
-
 set -e
 
+echo "a"
 . ../../../node_modules/@musical-patterns/cli/bin/non_cli/share_file.sh
-
+echo "b"
 share_file Makefile.snapshot
+echo "c"
 share_file test/snapshot.test.ts
+echo "d"
