ffa544f363fcc427e2ff44b74294c72dc24dc320
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-30 14:38:31 -0800

    do not like linting that

--- src/filter.ts
+++ src/filter.ts
@@ -30,7 +30,6 @@ const filter: PatternsFilter =
         })
 
         const filteredIds: Id[] = ids
-        // tslint:disable-next-line:no-misleading-array-reverse
             .sort()
             .filter((id: Id): boolean =>
                 idsToFilter.every((filteredId: Id): boolean =>
