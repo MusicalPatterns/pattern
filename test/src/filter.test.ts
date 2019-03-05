325d6840dceaa40efd35461d5f8cf2f7d00dbf18
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-04 17:47:47 -0800

    1.0.183: accumulators improvements

--- test/src/filter.test.ts
+++ test/src/filter.test.ts
@@ -3,14 +3,13 @@ import { filter, Id, Pattern, Patterns } from '../../src/indexForTest'
 
 describe('filter', () => {
     it('removes the development-only patterns', () => {
-        const patternAccumulator: Patterns = {}
         const patterns: Patterns = keys(Id)
             .reduce(
                 (accumulator: Patterns, id: keyof typeof Id): Patterns => ({
                     ...accumulator,
                     [ id ]: {},
                 }),
-                patternAccumulator,
+                {},
             )
 
         const filteredPatterns: Patterns = filter(patterns)
